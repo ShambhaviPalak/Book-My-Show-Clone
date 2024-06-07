b1a191aa712a050b4a4b04df3a663ec9df14bea4
ShambhaviPalak <shambhavipalak02.gmail.com>
2024-06-07 20:26:07 +0530

    Book My Show Clone

---
+++ plan.js
@@ -0,0 +1,21 @@
+//book my show planning
+
+// we need a resuable carousel
+//HERO CAROUSEL
+//COMMON COROUSEL
+//CAST COROUSEL(SMALL CAROUSEL)
+
+//PAGES
+
+//home pg
+//movie pg
+//categories pg
+//authentication pg
+//razorpay payment gateway
+
+//PLAN HOW WE ARE BUILDING IT?
+
+//layouts
+//routes
+//PAGES
+//components
